2d98e9da7baa40a2ff76a4bcef7e544424888152
Christophe Sonntag <15939808+c-sonntag@users.noreply.github.com>
2018-07-21 21:32:04 +0200

    [Versioning]

---
+++ Readme.md
@@ -0,0 +1,6 @@
+
+
+
+## About file dependencies
+
+@todo File are marked by date-modifaication value, you can force modification watch with flag "checksum" 
--- tests/relative_files/good_maker_listener.cpp
+++ tests/relative_files/good_maker_listener.cpp
@@ -5,6 +5,7 @@
 #include <gtest/gtest.h>
 
 #include <vector>
+#include <array>
 #include <string>
 #include <exception>
 
@@ -22,17 +23,34 @@ TEST( relative_files, good_maker_listener )
   ASSERT_EQ( files.files_not_found.size(), 2 );
 
   //
-  //ASSERT_EQ( files.files_found[0].property, erc::file_property( "file1.txt", ".txt", 5 ) );
-  //ASSERT_EQ( files.files_found[1].property, erc::file_property( "file2.txt", ".txt", 5 ) );
-  //ASSERT_EQ( files.files_found[2].property, erc::file_property( "file3.txt", ".txt", 3 ) );
-  //ASSERT_EQ( files.files_found[3].property, erc::file_property( "file4.txt", ".txt", 8 ) );
-  //ASSERT_EQ( files.files_found[4].property, erc::file_property( "file5.txt", ".txt", 9 ) );
-  //ASSERT_EQ( files.files_found[5].property, erc::file_property( "sub_file1.txt", ".txt", 11 ) );
-  //ASSERT_EQ( files.files_found[6].property, erc::file_property( "sub_file2.txt", ".txt", 12 ) );
+  const struct { std::string path; erc::file_property property; } desired_files_found[7]
+  {
+    {"file1.txt",             { "file1.txt",     ".txt", 5  }},
+    {"file2.txt",             { "file2.txt",     ".txt", 5  }},
+    {"file3.txt",             { "file3.txt",     ".txt", 3  }},
+    {"file4.txt",             { "file4.txt",     ".txt", 8  }},
+    {"file5.txt",             { "file5.txt",     ".txt", 9  }},
+    {"sub_dir/sub_file1.txt", { "sub_file1.txt", ".txt", 11 }},
+    {"sub_dir/sub_file2.txt", { "sub_file2.txt", ".txt", 12 }},
+  };
+  for ( const auto & dff : desired_files_found )
+  {
+    const auto f_found_it( files.files_found.find( dff.path ) );
+    ASSERT_TRUE( f_found_it != files.files_found.end() );
+    if ( f_found_it != files.files_found.end() )
+      ASSERT_EQ( f_found_it->second.property, dff.property );
+  }
 
   //
-  ASSERT_EQ( files.files_not_found[0], "nop1" );
-  ASSERT_EQ( files.files_not_found[1], "nop2" );
+  const std::string desired_files_not_found[2]
+  {
+    "nop1", "nop2",
+  };
+  for ( const auto & dfnf : desired_files_not_found )
+  {
+    const auto nf_found_it( files.files_not_found.find( dfnf ) );
+    ASSERT_TRUE( nf_found_it != files.files_not_found.end() );
+  }
 
 
 }
